5eef707e1aa818c5b5dffbcb92c0e42d79e68b5d
zenklot <zenklot11@gmail.com>
2021-11-24 07:42:11 +0700

    add port

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/zenklot/backend-zenknote/database"
+	"github.com/zenklot/backend-zenknote/helper"
 	"github.com/zenklot/backend-zenknote/router"
 )
 
@@ -13,8 +14,14 @@ func main() {
 	app := fiber.New()
 	app.Use(cors.New())
 	database.ConnectDB()
-
+	var port string
+	port = helper.Config("PORT")
+	if port != "" {
+		port = ":" + port
+	} else {
+		port = ":3000"
+	}
 	router.SetupRoutes(app)
-	log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(port))
 	// defer database.DB.Close()
 }
